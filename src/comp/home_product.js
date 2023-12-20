d40c4b5b98920cda9904ab0abed71eb435de15bc
Anuj Kumar <anujkumarsingh065@gmail.com>
2023-12-20 23:23:08 +0530

    changed link

--- src/comp/home_product.js
+++ src/comp/home_product.js
@@ -4,7 +4,7 @@ const Homeproduct =
             id: 1,
             Name: 'product1',
             price: '26',
-            image: 'image/p1.webp',
+            image: 'https://anujkumarsingh065.github.io/AppScrip-task-Anuj_Kumar_Singh/image/p1.webp',
             cat: 'tv',
             type: 'new',
         },
